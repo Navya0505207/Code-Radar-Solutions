9740cbf9a1e4475d0b8ac206e520dc64956d01ee
Navya <iamnavya05@gmail.com>
2025-02-14 18:21:04 +0530

    Solution to Grade Evaluation

--- Grade Evaluation@@6788dd63490637cfc44e1bab/code.c
+++ Grade Evaluation@@6788dd63490637cfc44e1bab/code.c
@@ -7,7 +7,7 @@ int main(){
     scanf("%c", &a);
    
 
-    if(a==A){printf("Excellent");}
+    if(a=='A'){printf("Excellent");}
     else if(a=='B'){printf("Good");}
      else if(a=='C'){printf("Average");}
      else if (a=='D') {printf("Below Average");}
