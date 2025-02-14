d6c31e694e3d6f26ee0eb57d5d21f8345334622f
Navya <iamnavya05@gmail.com>
2025-02-14 17:57:56 +0530

    Solution to Simple Calculator

---
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -0,0 +1,12 @@
+
+#include<stdio.h>
+int main(){
+    int a,b;
+    char c;
+    scanf("%d%d%c",&a,&b,&c);
+
+    if(c=='+'){printf("%d%d",a+b);}
+    
+    else{printf("Different Sign");}
+    return 0;
+}
